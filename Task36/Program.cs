4367d8fc17fd2e84304d8b4e087b931c1fae1625
unknown <volk.3894@yandex.ru>
2022-09-08 19:24:06 +0300

    save1

---
+++ Task36/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+Console.WriteLine("Введите число: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+int count = 0;
+
+for (int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(-20,20);
+    if (i % 2 != 0)
+    {
+        count+=array[i];
+    }
+}
+
+Console.Write("[{0}]",string.Join("; ",array));
+Console.WriteLine(" -> "+count);
---
+++ Task38/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+Console.WriteLine("Введите число: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+int max = array[0];
+int min = 100;
+
+
+for (int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(0,100);
+
+}    
+
+for (int i=0; i<size; i++)
+{
+if(array[i] > max)
+    {
+        max = array[i];
+    }
+
+}
+
+for (int j=0; j<size; j++)
+{
+if(array[j] < min)
+    {
+        min = array[j];
+    }
+
+}
+
+Console.Write("[{0}]",string.Join("; ",array));
+Console.WriteLine(" -> " + (max-min));
+
